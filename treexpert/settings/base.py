c79d70178a4cb47ea71f6fbcad9ed69d634e86ae
Paula Ritter <paula.ritter@dfki.de>
2023-12-01 12:28:06 +0100

    add tree app to settings

--- treexpert/settings/base.py
+++ treexpert/settings/base.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "core.apps.CoreConfig",
+    "tree.apps.TreeConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",

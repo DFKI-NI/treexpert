aff25fd5a822727b4f073501bef4edbaf1cd72b1
Paula Kammler <paula.ritter@dfki.de>
2023-12-06 15:24:54 +0100

    update pipeline to use coverage.py and new settings

---
+++ treexpert/settings/ci.py
@@ -0,0 +1,12 @@
+from .base import *
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "ci",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "postgres",
+        "PORT": "5432",
+    },
+}
